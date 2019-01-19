d6e4fcc1c1f498103925ac2569ae7fc64cfe4429
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 21:19:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
-def my_collect (array)
+def my_collect (array, method)
   i = 0
+
 
   while i < array.length
     yield array[i]
+    array[i] = method.call(array[i])
     i += 1
   end
+  array
 end
